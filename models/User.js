65d185afc50143e96a2b18be3c45e851ca5152d4
Mohak Parekh <mkparekh2803@gmail.com>
2020-06-13 17:52:55 +0530

    Default models

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+  },
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String,
+  },
+  twitter: {
+    id: String,
+    name: String,
+    email: String,
+    token: String,
+  },
+  linkedIn: {
+    id: String,
+    name: String,
+    email: String,
+    token: String,
+  },
+  google: {
+    id: String,
+    name: String,
+    email: String,
+    token: String,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
