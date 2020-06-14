c809355c5fe9f1e2d4bde560dc711f9eae5bd2e2
mana9512 <manashreepatel28@gmail.com>
2020-06-15 00:25:52 +0530

    new file added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,80 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const config = require("config");
+const passport = require('passport');
 
 const router = express.Router();
+
+const LocalStrategy = require('passport-local').Strategy;
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getUserByUsername(username, function(err, user){
+      if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'Unknown User'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch){
+        if(err) throw err;
+     	if(isMatch){
+     	  return done(null, user);
+     	} else {
+     	  return done(null, false, {message: 'Invalid password'});
+     	}
+     });
+   });
+  }
+));
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+router.post('/register', function(req, res){
+    var password = req.body.password;
+    var password2 = req.body.password2;
+  
+    if (password == password2){
+      var newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+      });
+  
+      User.createUser(newUser, function(err, user){
+        if(err) throw err;
+        res.send(user).end()
+      });
+    } else{
+      res.status(500).send("{errors: \"Passwords don't match\"}").end()
+    }
+  });
+  
+router.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    res.send(req.user);
+  }
+);
+
+
+
+// Endpoint to get current user
+router.get('/', function(req, res){
+  res.send(req.user);
+})
+
+
+// Endpoint to logout
+router.get('/logout', function(req, res){
+  req.logout();
+  res.send(null)
+});
 
 module.exports = router;
